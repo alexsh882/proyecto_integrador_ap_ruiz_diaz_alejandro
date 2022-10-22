794009ba6eb7d639e11077da45feabc2e80ba4f0
Alejandro <alexsh882@gmail.com>
2022-10-22 10:59:51 -0300

    Inicial Commit Backend

---
+++ backend/src/main/java/com/argentinaprogramaanrd/anrd/AnrdApplication.java
@@ -0,0 +1,13 @@
+package com.argentinaprogramaanrd.anrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnrdApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/argentinaprogramaanrd/anrd/AnrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argentinaprogramaanrd.anrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
